215ad1eb769cad640f7f77d6aa2ec6ab82a56796
Joshua DeHaan <joshdehaan2@gmail.com>
2023-04-04 15:14:12 -0400

    initial commit

---
+++ app/root.jsx
@@ -0,0 +1,44 @@
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+
+import sharedStyles from "./styles/shared.css";
+import MainHeader from "./components/navigation/MainHeader";
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <MainHeader />
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
+export const links = () => {
+  return [{ rel: "stylesheet", href: sharedStyles }];
+};
---
+++ app/routes/expenses.raw.jsx
@@ -0,0 +1,7 @@
+export default function RawExpenses() {
+  return (
+    <div>
+      <h1>Raw Works</h1>
+    </div>
+  );
+}
